05887b4b1b8504460544f3bedf9bcb24aa898e3d
Douglas Sjoquist <dwsjoquist@sunetos.com>
2016-04-04 18:11:56 -0400

    Turn asda branch into framework

---
+++ SwiftFiniteStateMachine/SwiftFiniteStateMachine/SwiftFiniteStateMachine.h
@@ -0,0 +1,19 @@
+//
+//  SwiftFiniteStateMachine.h
+//  SwiftFiniteStateMachine
+//
+//  Created by Douglas Sjoquist on 4/4/16.
+//  Copyright © 2016 Ivy Gulch LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftFiniteStateMachine.
+FOUNDATION_EXPORT double SwiftFiniteStateMachineVersionNumber;
+
+//! Project version string for SwiftFiniteStateMachine.
+FOUNDATION_EXPORT const unsigned char SwiftFiniteStateMachineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFiniteStateMachine/PublicHeader.h>
+
+
